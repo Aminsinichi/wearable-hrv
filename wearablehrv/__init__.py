3c1556a3d08a8f9f844f6673d8de1798fbe4d0ec
Amin <aminsinichi@gmail.com>
2024-07-26 11:10:45 +0200

    update setup.py and __initi__.py

--- wearablehrv/__init__.py
+++ wearablehrv/__init__.py
@@ -2,7 +2,7 @@
 #### A Python package designed for data preparation, pre-processing, feature exctraction, comparison, visualization, and statistical individual/group analysis of heart rate and heart rate variability outcome variables recorded from wearable devices that transmit raw interbeat intervals to a host device via Bluetooth.
 ############################ Wearablehrv ############################
 
-__version__ = "0.1.14"
+__version__ = "0.2.0"
 
 from . import individual
 from . import group
